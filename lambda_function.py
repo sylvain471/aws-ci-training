49e30c4494e2968ec81bf6f591bc719307a8fefb
sylvain471 <sylvain.d@471.fr>
2024-12-06 15:19:35 +0100

    fix typos lambda_function

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event,context):
     print("Recieved event:"+json.dumps(event,indent=2))
-    message="Hello from lambda"
+    message="Hello from lambda!"
     return {
         "statusCode":200,
         "body":json.dumps({
